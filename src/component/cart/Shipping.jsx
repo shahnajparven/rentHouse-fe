e48afe688d18783fac4c68dd101268b63d09ca14
shahnaj parven <shahnajritu21@gmail.com>
2024-03-12 22:48:27 +0600

    change

--- src/component/cart/Shipping.jsx
+++ src/component/cart/Shipping.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import { Fragment, useState } from "react";
 import "./Shipping.css";
 import { useSelector, useDispatch } from "react-redux";
 import { saveShippingInfo } from "../../actions/cartAction";
